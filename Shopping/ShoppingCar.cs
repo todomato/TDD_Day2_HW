12839f0b169881691de28088f273983b353220f3
todomato <dreamrrrr@hotmail.com>
2017-01-16 18:08:25 +0800

    新增測試情境 : Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ Shopping/ShoppingCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace HarryBooks
+{
+    public class ShoppingCar
+    {
+        private List<Shopping.Book> Books;
+
+        public ShoppingCar(List<Shopping.Book> Books)
+        {
+            // TODO: Complete member initialization
+            this.Books = Books;
+        }
+
+        public int TotalCost { get; set; }
+
+        public int CalculateCost()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ShoppingTests/ShoppingCarTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shopping;
+using System.Collections.Generic;
+using ExpectedObjects;
+
+namespace HarryBooks.Tests
+{
+    [TestClass()]
+    public class ShoppingCarTests
+    {
+        [TestMethod()]
+        public void Buy_One_First_Eposide_Price_Should_100()
+        {
+            var Books = new List<Book>()
+            {
+                new Book(){ Name = "Harry", Episode = 1, Count = 1, UnitPrice = 100}
+            };
+            var shoppingCar = new ShoppingCar(Books);
+            var excepted = 100;
+            
+            shoppingCar.CalculateCost();
+
+            excepted.ToExpectedObject().ShouldEqual(shoppingCar.TotalCost);
+        }
+    }
+}
