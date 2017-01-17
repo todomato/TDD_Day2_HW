99bc847d16b46c1010b19627e3961e06951af58e
todomato <dreamrrrr@hotmail.com>
2017-01-17 13:16:51 +0800

    實做+重構 shoppingCar.CalculateCost

--- Shopping/ShoppingCar.cs
+++ Shopping/ShoppingCar.cs
@@ -19,30 +19,48 @@ namespace HarryBooks
 
         public void CalculateCost()
         {
-            // 用集數分類
-            var differentCount = _books.GroupBy(g => g.Episode).Count();
+            // 不同集數數量
+            var differentCount = _books.Count();
 
-            // 判斷
+            // 打折邏輯
             if (differentCount == 5)
             {
                 SumPrice(_books.Select(c => c.Episode), 0.75);
+                RemoveCalculatedBooks();
             }
             else if (differentCount == 4)
             {
                 SumPrice(_books.Select(c => c.Episode), 0.8);
+                RemoveCalculatedBooks();
             }
             else if (differentCount == 3)
             {
                 SumPrice(_books.Select(c => c.Episode), 0.9);
+                RemoveCalculatedBooks();
             }
             else if (differentCount == 2)
             {
                 SumPrice(_books.Select(c => c.Episode), 0.95);
+                RemoveCalculatedBooks();
             }
-            else 
+            else if (differentCount == 1)
             {
                 SumPrice(_books.Select(c => c.Episode), 1);
             }
+        }
+
+
+        /// <summary>
+        /// 移除已計算的書籍,然後繼續運算(遞迴)
+        /// </summary>
+        private void RemoveCalculatedBooks()
+        {
+            foreach (var item in _books)
+            {
+                item.Count += -1;
+            }
+            _books = _books.Where(c => c.Count != 0).ToList();
+            CalculateCost();
         }
 
         /// <summary>
@@ -52,7 +70,7 @@ namespace HarryBooks
         /// <param name="discount">折扣</param>
         private void SumPrice(IEnumerable<int> enumerable, double discount)
         {
-            _totalCost = (int)(_books.Sum(c => c.UnitPrice) * discount);
+            _totalCost += (int)(_books.Sum(c => c.UnitPrice) * discount);
         }
 
    
