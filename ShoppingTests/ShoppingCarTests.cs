d90fc2a506e346372a9e349670cfbb745dca41c9
todomato <dreamrrrr@hotmail.com>
2017-01-17 11:38:20 +0800

    新增測試情境6: 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- ShoppingTests/ShoppingCarTests.cs
+++ ShoppingTests/ShoppingCarTests.cs
@@ -75,7 +75,7 @@ namespace HarryBooks.Tests
         }
 
         [TestMethod()]
-        public void Buy_1_First_Episode_And_Second_Episode_And_Third_Episode_Price_And_4th_Episode_Price_And_5th_Episode_Price_Should_375()
+        public void Buy_1_First_Episode_And_Second_Episode_And_Third_Episode_And_4th_Episode_Price_And_5th_Episode_Price_Should_375()
         {
             var Books = new List<Book>()
             {
@@ -87,6 +87,23 @@ namespace HarryBooks.Tests
             };
             var shoppingCar = new ShoppingCar(Books);
             var excepted = 375;
+
+            shoppingCar.CalculateCost();
+
+            excepted.ToExpectedObject().ShouldEqual(shoppingCar.TotalCost);
+        }
+
+        [TestMethod()]
+        public void Buy_1_First_Episode_And_Second_Episode_And_Buy_2_Thrid_Episode_Price_Should_370()
+        {
+            var Books = new List<Book>()
+            {
+                new Book(){ Name = "Harry", Episode = 1, Count = 1, UnitPrice = 100},
+                new Book(){ Name = "Harry", Episode = 2, Count = 1, UnitPrice = 100},
+                new Book(){ Name = "Harry", Episode = 3, Count = 2, UnitPrice = 100}
+            };
+            var shoppingCar = new ShoppingCar(Books);
+            var excepted = 370;
 
             shoppingCar.CalculateCost();
 
